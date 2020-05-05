22a3f6c341687a9d3f8ea7c0d18ec16e73159f8f
zsoltsarosi <zsolt.sarosi@gmail.com>
2020-05-05 10:36:57 +0200

    Create CountryDataController.cs

---
+++ covid19tracker/Controllers/CountryDataController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using covid19tracker.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace covid19tracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryDataController : ControllerBase
+    {
+        private readonly CountryAggregatedContext _db;
+        private readonly ILogger<CountryDataController> _logger;
+
+        public CountryDataController(CountryAggregatedContext db, ILogger<CountryDataController> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        // GET: api/countrydata
+        [HttpGet]
+        public async Task<ActionResult<IList<CountryAggregated>>> GetEntries()
+        {
+            var lastDateEntry = await _db.CountriesData.OrderByDescending(x => x.Date).FirstOrDefaultAsync();
+            if (lastDateEntry == null) // no entires in db
+            {
+                return Enumerable.Empty<CountryAggregated>().ToList();
+            }
+
+            var lastDateEntries = await _db.CountriesData.Where(x => x.Date == lastDateEntry.Date).OrderBy(x => x.Country).ToListAsync();
+            return lastDateEntries;
+        }
+    }
+}
