13f5e8b40973cdf6c63479be5cd9a511c81e80a6
zsoltsarosi <zsolt.sarosi@gmail.com>
2020-05-07 12:45:55 +0200

    Reduced properties json serialization names.

--- covid19tracker/Model/WorldAggregated.cs
+++ covid19tracker/Model/WorldAggregated.cs
@@ -1,6 +1,7 @@
 ﻿using CsvHelper.Configuration.Attributes;
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace covid19tracker.Model
 {
@@ -8,18 +9,24 @@ namespace covid19tracker.Model
     {
         [Key]
         [Index(0)]
+        [JsonPropertyName("t")]
+        // TODO: serialize only date part
         public DateTime Date { get; set; }
 
         [Index(1)]
+        [JsonPropertyName("c")]
         public int Confirmed { get; set; }
 
         [Index(2)]
+        [JsonPropertyName("r")]
         public int Recovered { get; set; }
 
         [Index(3)]
+        [JsonPropertyName("d")]
         public int Deaths { get; set; }
 
         [Index(4)]
+        [JsonPropertyName("i")]
         public float? IncreaseRate { get; set; }
     }
 }
