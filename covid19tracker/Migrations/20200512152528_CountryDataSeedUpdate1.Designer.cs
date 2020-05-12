fdd43905e4dd050a3403b5b9714ecdbb19709827
zsoltsarosi <zsolt.sarosi@gmail.com>
2020-05-12 17:38:21 +0200

    Countries data seed update.

--- covid19tracker/Migrations/20200512152528_CountryDataSeedUpdate1.Designer.cs
+++ covid19tracker/Migrations/20200512152528_CountryDataSeedUpdate1.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using covid19tracker.Model;
 
 namespace covid19tracker.Migrations
 {
     [DbContext(typeof(CountryContext))]
-    partial class CountryContextModelSnapshot : ModelSnapshot
+    [Migration("20200512152528_CountryDataSeedUpdate1")]
+    partial class CountryDataSeedUpdate1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
