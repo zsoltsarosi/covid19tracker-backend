a7f891fda401e2af9db14b4e2b1fb462d21f3d93
zsoltsarosi <zsolt.sarosi@gmail.com>
2020-05-21 15:11:58 +0200

    Create DefaultJsonSerializerOptions.cs

---
+++ covid19tracker/Json/DefaultJsonSerializerOptions.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Text.Json;
+
+namespace covid19tracker.Json
+{
+    public static class DefaultJsonSerializerOptions
+    {
+        public static JsonSerializerOptions Options => new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            IgnoreNullValues = true
+        };
+    }
+}
